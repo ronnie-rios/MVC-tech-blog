126c0c3e9434a3d588e452b52f78a8bf7bc82762
Ronnie Rios <ronniesmail14@gmail.com>
2021-06-24 15:59:22 -0500

    fixed location of js for views/login; fixed req.session for user routes for /login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -46,24 +46,31 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  // expects {username: 'Lernantino',  password: 'password1234'}
   User.create({
     username: req.body.username,
-    email: req.body.email,
+    
     password: req.body.password
   })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+  .then((dbUserData) => {
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json(dbUserData);
     });
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/login', (req, res) => {
-  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
-      email: req.body.email
+      username: req.body.username
     }
   }).then(dbUserData => {
     if (!dbUserData) {
